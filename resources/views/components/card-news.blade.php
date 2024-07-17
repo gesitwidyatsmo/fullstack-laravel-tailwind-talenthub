c1afe647a9cc71c7188d9dfa37b5493257af37e2
Gesit Widi Atmoko <gesitwidiatmoko@gmail.com>
2024-07-18 01:05:27 +0700

    feat: add card news component

---
+++ resources/views/components/card-news.blade.php
@@ -0,0 +1,46 @@
+<div
+    class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg"
+>
+    <div
+        class="relative mx-4 mt-4 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40"
+    >
+        <img
+            src="https://images.unsplash.com/photo-1444653614773-995cb1ef9efa?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="ui/ux review check"
+        />
+    </div>
+    <div class="px-6 pt-2 pb-3">
+        <div class="mb-2 flex items-center justify-between">
+            <h5
+                class="block font-sans text-sm font-medium leading-snug tracking-normal text-blue-gray-900 antialiased"
+            >
+                Teknologi | Desain
+            </h5>
+            <p
+                class="flex items-center gap-1.5 font-sans text-sm font-normal leading-relaxed text-blue-gray-900 antialiased"
+            >
+                17 Juli 2024
+            </p>
+        </div>
+        <div
+            class="block font-sans text-lg font-bold leading-snug tracking-normal text-blue-gray-900 antialiased mb-3"
+        >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+        <div
+            class="block font-sans text-base font-light leading-relaxed text-gray-700 antialiased line-clamp-3"
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fuga
+            eligendi eius consectetur culpa, labore repudiandae eveni...
+        </div>
+    </div>
+    <div class="p-6 pt-3">
+        <button
+            class="block w-full select-none rounded-lg bg-blue-900 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+            data-ripple-light="true"
+        >
+            Selengkapnya
+        </button>
+    </div>
+</div>
