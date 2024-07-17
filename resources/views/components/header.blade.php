e0f82c5c4836f28d26c0b235ba8cc65f335a90e7
Gesit Widi Atmoko <gesitwidiatmoko@gmail.com>
2024-07-18 01:03:50 +0700

    feat: add header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,117 @@
+<header class="bg-white">
+    <div class="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:px-8">
+        <div class="relative flex h-16 justify-between">
+            <div class="relative z-10 flex px-2 lg:px-0">
+                <div class="flex flex-shrink-0 items-center">
+                    <div class="font-extrabold text-black text-xl">Gesit</div>
+                </div>
+            </div>
+            <div
+                class="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0"
+            >
+                <div class="w-full sm:max-w-xs">
+                    <label for="search" class="sr-only">Search</label>
+                    <div class="relative">
+                        <div
+                            class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+                        >
+                            <svg
+                                class="h-5 w-5 text-gray-400"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                                    clip-rule="evenodd"
+                                />
+                            </svg>
+                        </div>
+                        <input
+                            id="search"
+                            name="search"
+                            class="block w-full rounded-full border-2 py-1.5 pl-10 pr-3 focus:text-gray-900 focus:ring-0 focus:placeholder:text-gray-500 sm:text-sm sm:leading-6"
+                            placeholder="Search"
+                            type="search"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div
+                class="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center"
+            >
+                <div class="relative ml-4 flex-shrink-0">
+                    <div>
+                        <button
+                            type="button"
+                            class="relative flex rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white ring-offset-2 ring-offset-gray-800"
+                            id="user-menu-button"
+                            aria-expanded="false"
+                            aria-haspopup="true"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                stroke-width="2"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M5.121 19.121a4 4 0 00.879 1.263A4 4 0 008 21h8a4 4 0 002.121-.616 4 4 0 00.879-1.263M12 14a4 4 0 110-8 4 4 0 010 8z"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav class="hidden lg:flex lg:space-x-5 lg:py-2" aria-label="Global">
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >News</a
+            >
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >Teknologi</a
+            >
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >Otomotif</a
+            >
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >Bola</a
+            >
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >Lifestyle</a
+            >
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >Property</a
+            >
+            <a
+                href="#"
+                class="text-gray-900 text-base font-bold"
+                aria-current="page"
+                >Food</a
+            >
+        </nav>
+    </div>
+</header>
