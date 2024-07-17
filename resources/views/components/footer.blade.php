832e2605a8fda276aea22118fccda8d18364f9aa
Gesit Widi Atmoko <gesitwidiatmoko@gmail.com>
2024-07-18 01:05:57 +0700

    feat: add footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8 mt-5 md:flex md:items-center md:justify-between py-6">
+	<span class="text-sm text-black sm:text-center">© 2024 <a href="/" class="hover:underline" target="_blank">Gesit™</a>. All Rights Reserved.
+    </span>
+		<ul class="flex flex-wrap items-center mt-3 sm:mt-0">
+			<li>
+				<a href="#" class="mr-4 text-sm text-black font-medium hover:underline md:mr-6">About</a>
+			</li>
+			<li>
+				<a href="#" class="mr-4 text-sm text-black font-medium hover:underline md:mr-6">Privacy
+					Policy</a>
+			</li>
+			<li>
+				<a href="#" class="mr-4 text-sm text-black font-medium hover:underline md:mr-6">Licensing</a>
+			</li>
+			<li>
+				<a href="#" class="text-sm text-black font-medium hover:underline">Contact</a>
+			</li>
+		</ul>
+</div>
