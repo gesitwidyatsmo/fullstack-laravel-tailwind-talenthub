df4f1b656c156b1d08e16f62c087ee5e3c6287c7
Gesit Widi Atmoko <gesitwidiatmoko@gmail.com>
2024-07-18 01:06:14 +0700

    feat: add new route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
 Route::get('/halo', function () {
     return 'Halo, Laravel!';
+});
+
+Route::get('/tailwind', function () {
+    return view('tailwind');
 });
